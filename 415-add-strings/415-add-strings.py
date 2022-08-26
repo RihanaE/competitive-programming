8b79937aab62e7280772dee0158242c607e7ab7b
RihanaE <111240652+RihanaE@users.noreply.github.com>
2022-08-26 16:32:43 +0300

    Time: 42 ms (89.79%), Space: 13.9 MB (62.39%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1= int(num1)
+        num2= int(num2)
+        
+        total=num1+num2
+        return str(total)
+        
