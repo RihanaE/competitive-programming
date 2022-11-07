0c7404bf5ebd55ab0f42b7322c94d88d919eb63f
RihanaE <111240652+RihanaE@users.noreply.github.com>
2022-11-07 21:15:36 +0300

    Time: 817 ms (28.55%), Space: 16.5 MB (98.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,27 +1,21 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        store = [0]
+        d = {0: 1}
+        s = 0
+        l = 0
         res = 0
-        d={0:1}
 
-        for i in nums:
-            store.append(store[-1] + i)
+        while l < len(nums):
+            s += nums[l]
+            if s - k in d.keys():
+                res += d[s - k]
 
-        l= 1
+            if s in d.keys():
+                d[s] += 1
 
-        while l < len(store):
-            if store[l] - k in d:
-                res =res + d[store[l] - k]
-                
-                
-                
-                    
-            if store[l] in d.keys():
-                    d[store[l]] +=1
-                    
             else:
-                d[store[l]] =1
-                
-            l +=1
-            
+                d[s] = 1
+
+            l += 1
+
         return res
