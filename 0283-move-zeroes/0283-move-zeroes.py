31be18b735247ce692dda94d8e998f597fc73af8
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-01-24 19:53:22 +0300

    Time: 312 ms (42.82%), Space: 15.7 MB (12.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,24 +2,48 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
     Do not return anything, modify nums in-place instead.
+    
     """
+        
         if len(nums) == 1:
             return nums
-        l = 0
-        r = 1
+        
+        else:
+            l = 0
+            r = 1
+            
+            while r < len(nums):
+                if l < r and nums[l] == 0 and nums[r] != 0:
+                    nums[l], nums[r] = nums[r] , nums[l]
+                    l += 1
+                    r += 1
+                    
+                elif l < r and nums[l] != 0:
+                    l += 1
+                    
+                else:
+                    r += 1
+                    
+                    
+                    
+                    
+#         if len(nums) == 1:
+#             return nums
+#         l = 0
+#         r = 1
 
-        while r < len(nums) and l < len(nums):
-            if l <= r and nums[l] == 0 and nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r += 1
+#         while r < len(nums) and l < len(nums):
+#             if l <= r and nums[l] == 0 and nums[r] != 0:
+#                 nums[l], nums[r] = nums[r], nums[l]
+#                 l += 1
+#                 r += 1
 
-            elif l > r and nums[l] == 0 and nums[r] != 0:
-                r +=1
+#             elif l > r and nums[l] == 0 and nums[r] != 0:
+#                 r +=1
 
-            if r < len(nums) and nums[r] == 0:
-                r += 1
+#             if r < len(nums) and nums[r] == 0:
+#                 r += 1
 
-            if l < len(nums) and nums[l] != 0:
-                l += 1
+#             if l < len(nums) and nums[l] != 0:
+#                 l += 1
 
