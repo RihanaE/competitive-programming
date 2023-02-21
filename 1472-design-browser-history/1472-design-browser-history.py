89fa82c6ab3d513d5148c0eb2f497279644377d7
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-02-21 17:54:33 +0300

    Time: 217 ms (89.81%), Space: 16.6 MB (70.41%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,23 +1,23 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.stack = [homepage]
+        self.store = {0:homepage}
         self.pointer = 0
+        self.length = 1
 
     def visit(self, url: str) -> None:
-        while self.stack and self.pointer != len(self.stack) - 1:
-            self.stack.pop()
-        
-        self.stack.append(url)
-        self.pointer = len(self.stack) - 1
+        self.store[self.pointer + 1] = url
+        self.pointer += 1
+        self.length = self.pointer + 1
+    
 
     def back(self, steps: int) -> str:
-        self.pointer = max(0, self.pointer - steps)
-        return self.stack[self.pointer]
+        self.pointer = max(self.pointer - steps, 0)
+        return self.store[self.pointer]
 
     def forward(self, steps: int) -> str:
-        self.pointer = min(len(self.stack) - 1, self.pointer + steps)
-        return self.stack[self.pointer]
+        self.pointer = min(self.pointer + steps, self.length - 1)
+        return self.store[self.pointer]
         
 
 
