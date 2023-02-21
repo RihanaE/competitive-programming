5bf5c6ec6fc82e39fc4139744570db9e304edc2d
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-02-21 18:10:04 +0300

    Time: 309 ms (26.52%), Space: 16.9 MB (10.15%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,23 +1,36 @@
+class Node():
+    def __init__(self, val= 0, next = None, prev = None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+        
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.store = {0:homepage}
-        self.pointer = 0
-        self.length = 1
+        self.head = Node(homepage)
+        self.home = self.head
+        
 
     def visit(self, url: str) -> None:
-        self.store[self.pointer + 1] = url
-        self.pointer += 1
-        self.length = self.pointer + 1
-    
+        temp = Node(url)
+        temp.prev = self.home
+        self.home.next = temp
+        self.home = self.home.next
 
     def back(self, steps: int) -> str:
-        self.pointer = max(self.pointer - steps, 0)
-        return self.store[self.pointer]
+
+        while steps and self.home.prev:
+            self.home = self.home.prev
+            steps -=1
+            
+        return self.home.val
 
     def forward(self, steps: int) -> str:
-        self.pointer = min(self.pointer + steps, self.length - 1)
-        return self.store[self.pointer]
+        while steps and self.home.next:
+            self.home = self.home.next
+            steps -=1
+            
+        return self.home.val
         
 
 
