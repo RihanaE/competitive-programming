e25eda9fb1c4cfc252733605a552465e1b935f8d
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-02-22 11:13:32 +0300

    Time: 126 ms (41.19%), Space: 13.9 MB (93.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,37 +1,25 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        d1={}
-        d2={}
-        for i in s1:
-            if i not in d1:
-                d1[i]=1
+        length = len(s1) - 1
+        pdict = Counter(s1)
+        temp = {}
+        left = 0
+    
+        for right in range(len(s2)):
+            if s2[right] in temp:
+                temp[s2[right]] += 1
                 
             else:
-                d1[i] +=1
+                temp[s2[right]] = 1
                 
-        j=len(s1)
-        l=r=0
-        
-        while r < len(s2):
-            if r - l + 1 <= j:
-                if s2[r] not in d2:
-                    d2[s2[r]]=1
-                    
-                else:
-                    d2[s2[r]] +=1
-                    
-                r +=1
-                
-            else:
-                if d1 == d2:
+            if right - left == length:
+                if temp == pdict:
                     return True
-                else:
-                    d2[s2[l]] -=1
-                    if d2[s2[l]] == 0:
-                        del(d2[s2[l]])
                     
-                    l +=1
+                temp[s2[left]] -= 1
+                if temp[s2[left]] == 0:
+                    del(temp[s2[left]])
                     
-        if d1 == d2:
-            return True
+                left += 1
+                
         return False
