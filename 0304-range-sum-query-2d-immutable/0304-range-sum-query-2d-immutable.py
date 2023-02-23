819d00bd8e5ef9dbe946caf817eab1ce3f6c5783
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-02-23 11:59:06 +0300

    Time: 1461 ms (64.58%), Space: 24.9 MB (40.87%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,31 +1,20 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        r, c=len(matrix), len(matrix[0])
-
-        self.s_matrix=[]
-
-        for i in range(r + 1):
-            self.s_matrix.append([0] * (c + 1))
-
-        for i in range(r):
-            pre=0
-            for j in range(c):
-                pre +=matrix[i][j]
-                abv =self.s_matrix[i][j + 1]
-                self.s_matrix[i + 1][j + 1]= pre + abv
-
+        # self.matrix = matrix
+        self.res = [[0] * (len(matrix[0]) + 1)]
+        
+        for i in range(len(matrix)):
+            pre = [0]
+            for j in range(len(matrix[0])):
+                pre.append(pre[-1] + matrix[i][j] + self.res[-1][j + 1] - self.res[-1][j])
+                
+            self.res.append(pre)
+            
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        r1, r2, c1, c2=row1 + 1, row2 + 1, col1 + 1, col2 + 1
-
-        BR=self.s_matrix[r2][c2]
-        abv=self.s_matrix[r1 - 1][c2]
-        l=self.s_matrix[r2][c1 - 1]
-        TL=self.s_matrix[r1 - 1][c1 - 1]
-
-        return BR - abv -l + TL
+        return self.res[row2 + 1][col2 + 1] - self.res[row2 + 1][col1] - self.res[row1][col2 + 1] + self.res[row1][col1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
