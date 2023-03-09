c872d69a48aefb37f30829aa54d4ec9fa8d2a5a6
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-03-09 17:00:56 +0300

    Time: 7225 ms (8.10%), Space: 13.8 MB (99.26%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -4,7 +4,6 @@ class Solution:
         n = len(cookies)
         max_ = float('inf')
         trial = [0] * k 
-        self.res = []
         
         def backtrack(trial, counter, n, cookies):
             nonlocal max_
@@ -16,17 +15,10 @@ class Solution:
             for i in range(k):
                 trial[i] += cookies[counter]
                 if max(trial) < max_:
-                    self.res.append(backtrack(trial, counter + 1, n , cookies))
+                    backtrack(trial, counter + 1, n , cookies)
+                    
                 trial[i] -= cookies[counter]
                 
-#             ans.append(cookies[counter])
-#             first = backtrack(ans, counter + 1, n , sum_, cookies, max_)
-            
-#             ans.pop()
-#             second = backtrack(ans, counter + 1, n, sum_, cookies, max_)
-            
-                   
-        # res = backtrack([], 0, n , sum_, cookies, max_)
-        # print(res)
-        out = backtrack(trial, 0 , n, cookies)
+
+        backtrack(trial, 0 , n, cookies)
         return max_
