660b28e3a39a6f9f1d36d48d0f4dc743c405c6a8
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-03-16 18:53:37 +0300

    Time: 122 ms (15.60%), Space: 13.9 MB (70.17%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,53 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        grid = [["."] * n for i in range(n)]
+        count = 0
+    
+        def helper( grid, row, n):
+            nonlocal count
+            
+            if row >= n:
+                count += 1
+                print(count)
+                return
+
+            for i in range(n):
+                if self.upper(row, i, grid) and self.leftdia(row, i, grid) and self.rightdia(row, i, grid, n):
+                    grid[row][i] = "Q"
+                    helper(grid, row + 1,n)
+                    grid[row][i] = "."
+                    
+        helper(grid, 0, n)
+        return count
+                    
+
+    def upper(self, row, col, grid):
+        for i in range(row, -1, -1):
+            if grid[i][col] != ".":
+                return False
+            
+        return True
+    
+    def leftdia(self, row, col, grid):
+        if row < 0:
+            return True
+        
+        if col < 0 :
+            return True
+        
+        if grid[row][col] != ".":
+            return False
+        
+        return self.leftdia(row - 1, col - 1, grid)
+            
+    
+    def rightdia(self, row, col, grid, n):
+        if col >= n:
+            return True
+        if row < 0:
+            return True
+        
+        if grid[row][col] != ".":
+            return False
+        
+        return self.rightdia(row - 1, col + 1, grid, n)
