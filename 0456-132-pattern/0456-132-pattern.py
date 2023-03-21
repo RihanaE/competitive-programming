82e382c53ec85431401fe083cbcea4759885f41b
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-03-21 18:37:14 +0300

    Time: 399 ms (31.02%), Space: 32 MB (66.88%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,42 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        
+        if len(nums) < 3:
+            return False
+
+        stack = []
+        mn = float('inf')
+
+        for i in range(len(nums)):
+            if not stack:
+                if mn > nums[i]:
+                    mn = nums[i]
+                    stack.append([nums[i], mn])
+
+                else:
+                    stack.append([nums[i], mn])
+
+            elif stack[-1][0] > nums[i]:
+                if mn > nums[i]:
+                    mn = nums[i]
+                stack.append([nums[i], mn])
+
+
+            else:
+
+                while stack and stack[-1][0] <= nums[i]:
+                    stack.pop()
+
+                if mn > nums[i]:
+                    mn = nums[i]
+
+                stack.append([nums[i], mn])
+
+            if len(stack) > 1 and stack[-2][1] != stack[-2][0] and stack[-2][1] < stack[-1][0]:
+                return True
+
+        if len(stack) > 1 and stack[-2][1] != stack[-2][0] and stack[-2][1] < stack[-1][0]:
+            return True
+
+        else:
+            return False
