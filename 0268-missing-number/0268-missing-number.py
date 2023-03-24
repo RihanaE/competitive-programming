bf27276f59c815e9a423526e07ea6a012ca3de4c
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-03-24 18:39:51 +0300

    Time: 166 ms (25.93%), Space: 15.1 MB (67.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,24 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums) + 1):
-            if i not in nums:
-                return i
+        pointer = 0
+        track = {}
+        
+        while pointer < len(nums):
+            place = nums[pointer] - 1
+            
+            if nums[pointer] == 0:
+                track[0] = pointer
+                pointer += 1
+                
+                
+            elif place == pointer:
+                pointer += 1
+                
+            else:
+                nums[pointer], nums[place] = nums[place], nums[pointer]
+                
+        
+        if track:
+            return track[0] + 1
+        else:
+            return 0
