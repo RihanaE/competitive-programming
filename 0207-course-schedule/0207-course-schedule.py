f131ede95b75e35a5bcf4bd968fd9f1c02a09904
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-05-10 17:56:28 +0300

    Time: 123 ms (13.22%), Space: 17.9 MB (17.21%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        weight = defaultdict(int)
+        store = defaultdict(list)
+        visited = set()
+        
+        for pre, next_ in prerequisites:
+            store[pre].append(next_)
+            weight[next_] += 1
+            
+        queue = deque()
+        
+        
+        for i in range(numCourses):
+            
+            if weight[i] == 0:
+                queue.append(i)
+                visited.add(i)
+                
+                
+        while queue:
+            node = queue.popleft()
+            
+            for neigh in store[node]:
+                if neigh not in visited:
+                    weight[neigh] -= 1
+                    
+                    if weight[neigh] == 0:
+                        queue.append(neigh)
+                        visited.add(neigh)
+                        
+      
+        return len(visited) == numCourses
