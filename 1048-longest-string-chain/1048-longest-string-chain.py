4c7044f33510968511de89acf18322cd574b18c9
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-09-24 00:30:29 +0300

    Time: 121 ms (83.24%), Space: 16.7 MB (93.47%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=len)
+
+        longest_chain_length = {}
+
+        max_chain_length = -1
+
+        for word in words:
+            longest_chain_length[word] = 1
+
+            for i in range(len(word)):
+                reduced_word = word[:i] + word[i + 1:]
+
+                if reduced_word in longest_chain_length:
+                    longest_chain_length[word] = max(longest_chain_length[word], longest_chain_length[reduced_word] + 1)
+
+            max_chain_length = max(max_chain_length, longest_chain_length[word])
+
+        return max_chain_length
