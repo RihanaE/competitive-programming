3f98d9d1e31ae4444e7b3483dfe691d3c01717f2
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-10-13 19:14:08 +0300

    Time: 580 ms (76.11%), Space: 16.1 MB (99.53%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,50 +1,16 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        leftArray = [0] * len(rating)
-        rightArray = [0] * len(rating)
         
-        
-        rightPointer = 1
-        
-        while rightPointer < len(rating):
-            count = 0
-            for leftPointer in range(rightPointer):
-                if rating[leftPointer] < rating[rightPointer]:
-                    count += 1
-                    
-            leftArray[rightPointer] = count
-            rightPointer += 1
-            
-        leftPointer = len(rating) - 2
-        
-        while leftPointer >= 0:
-            count = 0
-            
-            for rightPointer in range(len(rating) - 1, leftPointer, -1):
-                if rating[leftPointer] > rating[rightPointer]:
-                    count += 1
-                    
-            rightArray[leftPointer] = count
-            leftPointer -= 1
-            
-        res = 0
-        
-        rightPointer = 1
-        
-        while rightPointer < len(rating):
-            for leftPointer in range(rightPointer):
-                if rating[leftPointer] < rating[rightPointer]:
-                    res += leftArray[leftPointer]
-                    
-            rightPointer += 1
-            
-        leftPointer = len(rating) - 2
-        
-        while leftPointer >= 0:
-            for rightPointer in range(len(rating) - 1, leftPointer, -1):
-                if rating[leftPointer] > rating[rightPointer]:
-                    res += rightArray[rightPointer]
-                    
-            leftPointer -= 1
-            
-        return res
+        count = 0
+        increasing = [0]* len(rating)
+        decreasing = [0] * len(rating)
+
+        for i in range(len(rating)-1, -1, -1):
+            for j in range(i + 1, len(rating)):
+                if rating[i] < rating[j]:
+                    increasing[i] += 1
+                    count += increasing[j]
+                else:
+                    decreasing[i] += 1
+                    count += decreasing[j]
+        return count
