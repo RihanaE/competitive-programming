b5a4b6d847374f04d4331c25b041cb412fa52173
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-11-23 00:08:36 +0300

    Time: 639 ms (22.25%), Space: 16.4 MB (73.09%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -2,21 +2,40 @@ class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         res = []
         
-        def helper(num):
-            step = 0
-            while num != 1:
-                if num % 2 == 0:
-                    num = num // 2
-                    
-                else:
-                    num = (3 * num) + 1
-                    
-                step += 1
+        def helper(num, step):
+            if num == 1:
+                return step
+            
+            if num % 2 == 0:
+                return helper(num // 2, step + 1)
                 
-            return step
-        
+            else:
+                return helper((3 * num) + 1, step + 1)
+                
         for i in range(lo, hi + 1):
-            res.append([helper(i), i])
+            res.append([helper(i, 0), i])
             
         res.sort()
         return res[k - 1][1]
+        
+        
+#         res = []
+        
+#         def helper(num):
+#             step = 0
+#             while num != 1:
+#                 if num % 2 == 0:
+#                     num = num // 2
+                    
+#                 else:
+#                     num = (3 * num) + 1
+                    
+#                 step += 1
+                
+#             return step
+        
+#         for i in range(lo, hi + 1):
+#             res.append([helper(i), i])
+            
+#         res.sort()
+#         return res[k - 1][1]
