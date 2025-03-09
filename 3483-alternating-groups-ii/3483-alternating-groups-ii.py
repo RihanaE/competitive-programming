9410c5b3c1afdcaac517988e5e5891862fd10ce5
RihanaE <111240652+RihanaE@users.noreply.github.com>
2025-03-09 23:34:21 +0300

    Time: 1076 ms (5.3%), Space: 21 MB (99.67%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.py
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -1,25 +1,21 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        n = len(colors)
-        # Initialize bad for the first window: positions 0 to k-1
-        bad = 0
-        for j in range(k - 1):
-            if colors[j % n] == colors[(j + 1) % n]:
-                bad += 1
-        
+        left = 0
+        right = 0
         res = 0
-        if bad == 0:
-            res += 1  # First window is alternating
-        
-        # Slide the window from i=1 to i=n-1
-        for i in range(1, n):
-            # Remove the pair that is leaving: (i-1, i)
-            if colors[(i - 1) % n] == colors[i % n]:
-                bad -= 1
-            # Add the new pair entering: (i+k-2, i+k-1)
-            if colors[(i + k - 2) % n] == colors[(i + k - 1) % n]:
-                bad += 1
-            if bad == 0:
+        length = len(colors)
+
+        while left < len(colors):
+            if right - left + 1 == k:
+                print(right, left)
+
                 res += 1
-        
+                left += 1
+
+            if colors[(right + 1) % length] == colors[right % length]:
+               
+                left = right + 1
+                
+            right += 1
+
         return res
