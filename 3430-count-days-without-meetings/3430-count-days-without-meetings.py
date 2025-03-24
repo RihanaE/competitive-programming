2347b3b2e8253f3b129b43dc01f67b8e0d0a6fea
RihanaE <111240652+RihanaE@users.noreply.github.com>
2025-03-24 23:02:28 +0300

    Time: 261 ms (12.72%), Space: 52.7 MB (86.51%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        interval = 0
+        print(meetings)
+        mn = float("inf")
+        mx = -float("inf")
+
+        for i in range(len(meetings)):
+            if i == 0:
+                interval += meetings[0][1] - meetings[0][0] + 1
+                mn = meetings[0][0]
+                mx = meetings[0][1]
+
+            else:
+                if meetings[i][0] <= mx and meetings[i][1] > mx:
+                    interval += meetings[i][1] - mx
+                    mx = meetings[i][1]
+
+                elif meetings[i][0] <= mx and meetings[i][1] <= mx:
+                    continue
+
+                else:
+                    interval += meetings[i][1] - meetings[i][0] + 1
+                    mx = meetings[i][1]
+                # [1, 4], [2, 5], [3, 4]
+        
+        return days - interval
