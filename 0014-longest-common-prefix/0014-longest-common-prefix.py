40f2e8bac1ca3cf5a175c65b842cba12f4741f43
RihanaE <111240652+RihanaE@users.noreply.github.com>
2025-07-16 17:30:24 +0300

    Time: 1 ms (37.29%), Space: 18.1 MB (8.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,19 +1,18 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        length=len(strs[0])
-        len_array=len(strs)
-        pointer=1
-        if len(strs) == 1:
-            return strs[0]
-        
-        else:
-            s=""
-            while pointer <= length:
-                for i in range(1,len_array):
-                    if strs[0][:pointer] != strs[i][:pointer]:
-                        return s
-                    
-                s=strs[0][:pointer]        
-                pointer +=1
-                
-            return s
+        res = ""
+        lim = len(strs[0])
+
+        for i in range(lim):
+            w = strs[0][i]
+
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]):
+                    return res
+
+                elif w != strs[j][i]:
+                    return res
+
+            res += w
+
+        return res
